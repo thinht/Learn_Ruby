d7a2a98728ce7de7ac087dcd04e24ee518b8362d
Thinh Tran <thinht@nustechnology.com>
2018-03-22 11:42:13 +0700

    Add config I18n

--- Exercise/Exercise_New/config/environments/development.rb
+++ Exercise/Exercise_New/config/environments/development.rb
@@ -57,4 +57,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.middleware.use I18n::JS::Middleware
 end
